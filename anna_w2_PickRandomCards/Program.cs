b9485eb6bdb80899e2c6b7879198f956b94aadb5
ChinHuaPan <80436165+ChinHuaPan@users.noreply.github.com>
2023-01-19 16:36:39 -0500

    Week 2 - new document

---
+++ anna_w2_PickRandomCards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace anna_w2_PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
