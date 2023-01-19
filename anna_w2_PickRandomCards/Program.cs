5e7a711377111c50708bb06a1b49d967e4fe9a46
ChinHuaPan <80436165+ChinHuaPan@users.noreply.github.com>
2023-01-19 17:44:33 -0500

    add comment

--- anna_w2_PickRandomCards/Program.cs
+++ anna_w2_PickRandomCards/Program.cs
@@ -8,19 +8,26 @@ namespace anna_w2_PickRandomCards
         {
             //string[] cards = CardPicker.PickSomeCards(5);
 
-            Console.Write("Enter the number of cards to pick: ");
-            string line = Console.ReadLine();
+            Console.Write("Enter the number of cards to pick: "); //show the sentence to ask the user to enter a number
 
-            if (int.TryParse(line, out int numberOfCards))
+            string line = Console.ReadLine(); //read the line that the user enters
+
+            //if the user enters a valid number
+            //parse "line" to int and pass to "numberOfCards"
+            if (int.TryParse(line, out int numberOfCards)) 
             {
+                //repeat for amount of "CardPicker.PickSomeCards(numberOfCards)" returns
+                //when "CardPicker.PickSomeCards(numberOfCards)" return something, pass it to card and do the following things
+                //until there is no return of "CardPicker.PickSomeCards(numberOfCards)"
                 foreach (string card in CardPicker.PickSomeCards(numberOfCards))
                 {
-                    Console.WriteLine(card);
+                    Console.WriteLine(card); //show "card" in the console
                 }
             }
+            //if the user enters an invalid number 
             else
             {
-                Console.WriteLine("Please enter a valid number.");
+                Console.WriteLine("Please enter a valid number."); //show the reminder
             }
 
         }
