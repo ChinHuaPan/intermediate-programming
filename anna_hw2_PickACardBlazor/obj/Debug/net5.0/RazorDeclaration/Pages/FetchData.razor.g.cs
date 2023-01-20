06fd73ff534a8370f7c52b5fe0503f9eb5e3c89e
ChinHuaPan <80436165+ChinHuaPan@users.noreply.github.com>
2023-01-19 19:38:19 -0500

    add comment

--- anna_hw2_PickACardBlazor/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ anna_hw2_PickACardBlazor/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,70 +13,70 @@ namespace anna_hw2_PickACardBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 1 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 2 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 3 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 4 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 5 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 6 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 7 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 8 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 9 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using anna_hw2_PickACardBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/_Imports.razor"
+#line 10 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/_Imports.razor"
 using anna_hw2_PickACardBlazor.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using anna_hw2_PickACardBlazor.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 37 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/anna_hw2_PickACardBlazor/Pages/FetchData.razor"
+#line 37 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/anna_hw2_PickACardBlazor/Pages/FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
