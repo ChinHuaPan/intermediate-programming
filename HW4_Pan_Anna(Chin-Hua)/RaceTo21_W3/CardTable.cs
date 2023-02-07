deaf57de67e79a437fbaf957478940edf69a03e9
ChinHuaPan <80436165+ChinHuaPan@users.noreply.github.com>
2023-02-06 19:07:38 -0500

    Fix the broken end conditions, allow the players to keep playing

---
+++ HW4_Pan_Anna(Chin-Hua)/RaceTo21_W3/CardTable.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RaceTo21
+{
+    public class CardTable
+    {
+        public CardTable()
+        {
+            Console.WriteLine("Setting Up Table...");
+        }
+
+        /* Shows the name of each player and introduces them by table position.
+         * Is called by Game object.
+         * Game object provides list of players.
+         * Calls Introduce method on each player object.
+         */
+        public void ShowPlayers(List<Player> players)
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                players[i].Introduce(i+1); // List is 0-indexed but user-friendly player positions would start with 1...
+            }
+        }
+
+        /* Gets the user input for number of players.
+         * Is called by Game object.
+         * Returns number of players to Game object.
+         */
+        public int GetNumberOfPlayers()
+        {
+            Console.Write("How many players? ");
+            string response = Console.ReadLine();
+            int numberOfPlayers;
+            while (int.TryParse(response, out numberOfPlayers) == false
+                || numberOfPlayers < 2 || numberOfPlayers > 8)
+            {
+                Console.WriteLine("Invalid number of players.");
+                Console.Write("How many players?");
+                response = Console.ReadLine();
+            }
+            return numberOfPlayers;
+        }
+
+        /* Gets the name of a player
+         * Is called by Game object
+         * Game object provides player number
+         * Returns name of a player to Game object
+         */
+        public string GetPlayerName(int playerNum)
+        {
+            Console.Write("What is the name of player# " + playerNum + "? ");
+            string response = Console.ReadLine();
+            while (response.Length < 1)
+            {
+                Console.WriteLine("Invalid name.");
+                Console.Write("What is the name of player# " + playerNum + "? ");
+                response = Console.ReadLine();
+            }
+            return response;
+        }
+
+        public bool OfferACard(Player player)
+        {
+            while (true)
+            {
+                Console.Write(player.name + ", do you want a card? (Y/N)");
+                string response = Console.ReadLine();
+                if (response.ToUpper().StartsWith("Y"))
+                {
+                    return true;
+                }
+                else if (response.ToUpper().StartsWith("N"))
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Please answer Y(es) or N(o)!");
+                }
+            }
+        }
+
+        public void ShowHand(Player player)
+        {
+            
+            if (player.cards.Count > 0)
+            {
+                Console.Write(player.name + " has: ");
+
+                //if it's the first one, we won't give it a comma
+                //I figured out this way and was inspired by this solution: https://stackoverflow.com/questions/43021/how-do-you-get-the-index-of-the-current-iteration-of-a-foreach-loop
+                var isFirst = true;
+                foreach (Card card in player.cards)
+                {
+                    if (isFirst)
+                    {
+                        Console.Write(card.displayName);
+                        isFirst = false;
+                    }
+                    else
+                    {
+                        Console.Write(", " + card.displayName);
+                    }
+                }
+                Console.Write(" = " + player.score + "/21 ");
+                if (player.status != PlayerStatus.active)
+                {
+                    Console.Write("(" + player.status.ToString().ToUpper() + ")");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ShowHands(List<Player> players)
+        {
+            foreach (Player player in players)
+            {
+                ShowHand(player);
+            }
+
+                Console.WriteLine("-----");
+            
+        }
+
+
+        public void AnnounceWinner(Player player)
+        {
+            if (player != null)
+            {
+                Console.WriteLine(player.name + " wins!");
+            }
+            else if (Game.everyOneIsStay)
+            {
+                Console.WriteLine("Come on, everyone...please be agressive!");
+            }
+            else
+            {
+                Console.WriteLine("Everyone busted!");
+            }
+            Console.Write("Play again...? (Y/N)");
+            //while (Console.ReadKey().Key != ConsoleKey.Enter) {
+
+                string response = Console.ReadLine();
+                if (response.ToUpper().StartsWith("Y"))
+                    {
+                    CardTable cardTable = new CardTable();
+                    Game game = new Game(cardTable);
+                    while (game.nextTask != Task.GameOver)
+                    {
+                        game.DoNextTask();
+                    }
+                }
+                else if (response.ToUpper().StartsWith("N"))
+                {
+                    return;
+                }
+            
+        }
+
+        public bool CheckBust(List<Player> players, int current)
+        {
+            bool otherAreBust = true;
+            for(int i=0; i < players.Count; i++)
+            {
+                if(i != current)
+                {
+                    otherAreBust = otherAreBust && (players[i].status == PlayerStatus.bust);
+                }
+            }
+
+            return otherAreBust;
+        }
+
+        public bool CheckStay(List<Player> players)
+        {
+
+            foreach (Player player in players)
+            {
+                    Game.everyOneIsStay = Game.everyOneIsStay && (player.status == PlayerStatus.stay);
+            }
+
+            return Game.everyOneIsStay;
+        }
+    }
+}
---
+++ HW4_Pan_Anna(Chin-Hua)/RaceTo21_W3/Game.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RaceTo21
+{
+    public class Game
+    {
+        int numberOfPlayers; // number of players in current game
+        List<Player> players = new List<Player>(); // list of objects containing player data
+        CardTable cardTable; // object in charge of displaying game information
+        Deck deck = new Deck(); // deck of cards
+        int currentPlayer = 0; // current player on list
+        static public bool everyOneIsStay = true;
+        public Task nextTask; // keeps track of game state
+        private bool cheating = true; // lets you cheat for testing purposes if true
+
+        public Game(CardTable c)
+        {
+            cardTable = c;
+            deck.Shuffle();
+            deck.ShowAllCards();
+            nextTask = Task.GetNumberOfPlayers;
+        }
+
+        /* Adds a player to the current game
+         * Called by DoNextTask() method
+         */
+        public void AddPlayer(string n)
+        {
+            players.Add(new Player(n));
+        }
+
+        /* Figures out what task to do next in game
+         * as represented by field nextTask
+         * Calls methods required to complete task
+         * then sets nextTask.
+         */
+        public void DoNextTask()
+        {
+            Console.WriteLine("================================"); // this line should be elsewhere right?
+            if (nextTask == Task.GetNumberOfPlayers)
+            {
+                numberOfPlayers = cardTable.GetNumberOfPlayers();
+                nextTask = Task.GetNames;
+            }
+            else if (nextTask == Task.GetNames)
+            {
+                for (var count = 1; count <= numberOfPlayers; count++)
+                {
+                    var name = cardTable.GetPlayerName(count);
+                    AddPlayer(name); // NOTE: player list will start from 0 index even though we use 1 for our count here to make the player numbering more human-friendly
+                }
+                nextTask = Task.IntroducePlayers;
+            }
+            else if (nextTask == Task.IntroducePlayers)
+            {
+                cardTable.ShowPlayers(players);
+                nextTask = Task.PlayerTurn;
+            }
+            else if (cardTable.CheckBust(players, currentPlayer))
+            {
+                OverGame();
+            }
+            else if (nextTask == Task.PlayerTurn)
+            {
+                cardTable.ShowHands(players);
+                Player player = players[currentPlayer];
+                if (player.status == PlayerStatus.active)
+                {
+                   if (cardTable.OfferACard(player))
+                    {
+                        Card card = deck.DealTopCard();
+                        player.cards.Add(card);
+                        player.score = ScoreHand(player);
+                        if (player.score > 21)
+                        {
+                            player.status = PlayerStatus.bust;
+                        }
+                        else if (player.score == 21)
+                        {
+                            player.status = PlayerStatus.win;
+                            nextTask = Task.CheckForEnd;
+
+                            OverGame();
+                        }
+                    }
+                    else
+                    {
+                        player.status = PlayerStatus.stay;
+                    }
+                }
+
+                if(nextTask != Task.GameOver)
+                {
+                    //cardTable.ShowHand(player);
+                    nextTask = Task.CheckForEnd;
+                }
+                
+            }
+            else if (nextTask == Task.CheckForEnd)
+            {
+                if (!CheckActivePlayers())
+                {
+                    //Player winner = DoFinalScoring();
+                    //cardTable.AnnounceWinner(winner);
+                    //nextTask = Task.GameOver;
+                    cardTable.CheckStay(players);
+                    OverGame();
+                }
+                else
+                {
+                    currentPlayer++;
+                    if (currentPlayer > players.Count - 1)
+                    {
+                        currentPlayer = 0; // back to the first player...
+                    }
+                    nextTask = Task.PlayerTurn;
+                }
+            }
+            else // we shouldn't get here...
+            {
+                Console.WriteLine("I'm sorry, I don't know what to do now!");
+                nextTask = Task.GameOver;
+            }
+        }
+
+        public int ScoreHand(Player player)
+        {
+            int score = 0;
+            if (cheating == true && player.status == PlayerStatus.active)
+            {
+                string response = null;
+                while (int.TryParse(response, out score) == false)
+                {
+                    Console.Write("OK, what should player " + player.name + "'s score be?");
+                    response = Console.ReadLine();
+                }
+                return score;
+            }
+            else
+            {
+                foreach (Card card in player.cards)
+                {
+                    string faceValue = card.id.Remove(card.id.Length-1);
+                    switch (faceValue)
+                    {
+                        case "K":
+                        case "Q":
+                        case "J":
+                            score = score + 10;
+                            break;
+                        case "A":
+                            score = score + 1;
+                            break;
+                        default:
+                            score = score + int.Parse(faceValue);
+                            break;
+                    }
+                }
+            }
+            return score;
+        }
+
+        public bool CheckActivePlayers()
+        {
+            foreach (var player in players)
+            {
+                if (player.status == PlayerStatus.active && player.status != PlayerStatus.win)
+                {
+                    return true; // at least one player is still going!
+                }
+            }
+            return false; // everyone has stayed or busted, or someone won!
+        }
+
+        public Player DoFinalScoring()
+        {
+            int highScore = 0;
+            foreach (var player in players)
+            {
+                cardTable.ShowHand(player);
+                if (player.status == PlayerStatus.win) // someone hit 21
+                {
+                    return player;
+                }
+                if (player.status == PlayerStatus.stay) // still could win...
+                {
+                    if (player.score > highScore)
+                    {
+                        highScore = player.score;
+                    }
+                }
+                
+                // if busted don't bother checking!
+            }
+
+            if (cardTable.CheckBust(players, currentPlayer))
+            {
+                highScore = players[currentPlayer].score;
+                return players[currentPlayer];
+            }
+
+            if (highScore > 0) // someone scored, anyway!
+            {
+                // find the FIRST player in list who meets win condition
+                return players.Find(player => player.score == highScore);
+            }
+
+            return null; // everyone must have busted because nobody won!
+        }
+
+        public void OverGame()
+        {
+            Player winner = DoFinalScoring();
+            cardTable.AnnounceWinner(winner);
+            nextTask = Task.GameOver;
+        }
+    }
+}
