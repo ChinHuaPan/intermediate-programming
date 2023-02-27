00ba9340769b342a309d7480233bfe55b09caffe
ChinHuaPan <80436165+ChinHuaPan@users.noreply.github.com>
2023-02-26 19:34:59 -0500

    move animals' movement from Game.cs to Animal.cs

--- W7_ZooManager_HW/ZooManager/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ W7_ZooManager_HW/ZooManager/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,85 +13,85 @@ namespace ZooManager.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 1 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 2 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 3 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 4 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 5 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 6 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 7 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 8 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 10 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using ZooManager.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/Pages/Index.razor"
+#line 2 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/Pages/Index.razor"
 using ZooManager;
 
 #line default
 #line hidden
 #nullable disable
-    [global::Microsoft.AspNetCore.Components.RouteAttribute("/")]
-    public partial class Index : global::Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/")]
+    public partial class Index : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
-        protected override void BuildRenderTree(global::Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
 #nullable restore
-#line 116 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/Pages/Index.razor"
+#line 116 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/Pages/Index.razor"
        
     protected override void OnInitialized()
     {
--- W7_ZooManager_HW/ZooManager/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ W7_ZooManager_HW/ZooManager/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace ZooManager.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 1 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 2 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 3 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 4 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 5 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 6 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 7 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 8 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 9 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using ZooManager;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/jay/NU/DGM6308_Winter2023/ZooManager_6_PostClass/ZooManager/_Imports.razor"
+#line 10 "/Users/chin-huapan/Documents/Northeastern University/2023winter_DGM6983_Intermediate Programming for Digital Media/intermediate-programming/W7_ZooManager_HW/ZooManager/_Imports.razor"
 using ZooManager.Shared;
 
 #line default
@@ -85,7 +85,7 @@ using ZooManager.Shared;
     public partial class MainLayout : LayoutComponentBase
     {
         #pragma warning disable 1998
-        protected override void BuildRenderTree(global::Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
