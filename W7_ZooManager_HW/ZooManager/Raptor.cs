87c1a163b49fa8da0d2f1630c2519e53131daeb1
ChinHuaPan <80436165+ChinHuaPan@users.noreply.github.com>
2023-02-26 20:32:14 -0500

    a/ create raptor

---
+++ W7_ZooManager_HW/ZooManager/Raptor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ZooManager
+{
+    public class Raptor : Animal
+    {
+        public Raptor()
+        {
+            emoji = "🦅";
+            species = "raptor";
+            this.name = name; // "this" to clarify instance vs. method parameter
+
+            reactionTime = new Random().Next(1, 4); // reaction time of 1 (fast) to 3
+            /* Note that Mouse reactionTime range is smaller than Cat reactionTime,
+             * so mice are more likely to react to their surroundings faster than cats!
+             */
+        }
+    }
+}
