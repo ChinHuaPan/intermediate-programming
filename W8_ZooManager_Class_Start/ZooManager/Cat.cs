5f9095e299cb532ed355e628554feadfacb05a90
ChinHuaPan <80436165+ChinHuaPan@users.noreply.github.com>
2023-02-28 17:19:48 -0500

    Week 8 starter + edit

---
+++ W8_ZooManager_Class_Start/ZooManager/Cat.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ZooManager
+{
+    public class Cat : Animal
+    {
+        public Cat(string name)
+        {
+            emoji = "🐱";
+            species = "cat";
+            this.name = name;
+            reactionTime = new Random().Next(1, 6); // reaction time 1 (fast) to 5 (medium)Cat
+        }
+
+        public override void Activate()
+        {
+            base.Activate();
+            Console.WriteLine("I am a cat. Meow.");
+            if (Flee()) return;
+            Hunt();
+        }
+
+        /* Note that our cat is currently not very clever about its hunting.
+         * It will always try to attack "up" and will only seek "down" if there
+         * is no mouse above it. This does not affect the cat's effectiveness
+         * very much, since the overall logic here is "look around for a mouse and
+         * attack the first one you see." This logic might be less sound once the
+         * cat also has a predator to avoid, since the cat may not want to run in
+         * to a square that sets it up to be attacked!
+         */
+        public void Hunt()
+        {
+            if (Game.Seek(location.x, location.y, Direction.up, "mouse"))
+            {
+                Game.Attack(this, Direction.up);
+            }
+            else if (Game.Seek(location.x, location.y, Direction.down, "mouse"))
+            {
+                Game.Attack(this, Direction.down);
+            }
+            else if (Game.Seek(location.x, location.y, Direction.left, "mouse"))
+            {
+                Game.Attack(this, Direction.left);
+            }
+            else if (Game.Seek(location.x, location.y, Direction.right, "mouse"))
+            {
+                Game.Attack(this, Direction.right);
+            }
+        }
+
+        public bool Flee()
+        {
+            if (Game.Seek(location.x, location.y, Direction.up, "elephant"))
+            {
+                if (Game.Retreat(this, Direction.down)) return true;
+            }
+            if (Game.Seek(location.x, location.y, Direction.down, "elephant"))
+            {
+                if (Game.Retreat(this, Direction.up)) return true;
+            }
+            if (Game.Seek(location.x, location.y, Direction.left, "elephant"))
+            {
+                if (Game.Retreat(this, Direction.right)) return true;
+            }
+            if (Game.Seek(location.x, location.y, Direction.right, "elephant"))
+            {
+                if (Game.Retreat(this, Direction.left)) return true;
+            }
+            return false;
+        }
+    }
+}
+
